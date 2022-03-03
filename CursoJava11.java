9e8bf625031a41f7a6a9a65fda7973b4baff7c50
joselucasoficial <joselucasoficial@gmail.com>
2022-03-03 15:57:58 -0300

    cj11

---
+++ CursoJava11.java
@@ -0,0 +1,19 @@
+public class CursoJava11 {
+    public static void main(String[] args) {
+        
+        final int tam = 5;
+        char[] gabarito = {'a', 'b', 'c', 'd', 'a'};
+        char[] respostas = new char[tam];
+        int nota;
+
+        respostas[0] = 'a';
+        respostas[1] = 'b';
+        respostas[2] = 'c';
+        respostas[3] = 'b';
+        respostas[4] = 'c';
+
+        for(int i = 0; i < tam; i++){
+            
+        }
+    }
+}
