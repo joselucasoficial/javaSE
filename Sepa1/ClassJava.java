5e747b1a4ecdb8e7f69b0aa9e55f1edd3b1d4f9e
joselucasoficial <joselucasoficial@gmail.com>
2022-03-16 00:12:18 -0300

    da22

--- Sepa1/ClassJava.java
+++ Sepa1/ClassJava.java
@@ -1,13 +1,17 @@
-package Sepa1;
 public class ClassJava {
     public static void main(String[] args) {
         
-        Player p1 = new Player(1); // new passa os valores de (Player para a vareavel (p))
-        Player p2 = new Player(2);
-        Player p3 = new Player(3);
+        int num = 0; // para iniciar a contagem dos jogadores
 
-        
-        System.out.printf("\n o player (%d) tem (%d) vidas", p1, p1.getLife() );
+        Player p1 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
+        System.out.printf("a quantidade de vidas do Player %d é %d\n", num, p1.getLife() );
+
+        Player p2 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
+        System.out.printf("a quantidade de vidas do Player %d é %d\n", num, p2.getLife() );
+
+        Player p3 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
+        System.out.printf("a quantidade de vidas do Player %d é %d\n",num, p3.getLife() );
+
 
     }
 }
--- Sepa1/Player.java
+++ Sepa1/Player.java
@@ -1,15 +1,15 @@
-package Sepa1;
-
 public class Player { // classe publica esta sendo acessada da classe ClassJava (main)
 
     private int num = 0;
-    private int life = 3;
+    private int life = 0;
 
     public Player(int num){  // classe construtora
         this.num = num;
+        this.life = 3; 
+        //System.out.printf("jogador numero %d criado \n", num);
     }
 
-    public int getLife(){
-        return this.life;
+    public int getLife(){ // metado tipo get pega o valor de alguma lugar
+        return this.life; // retorna o valor de life passado no construtor
     }
 }
