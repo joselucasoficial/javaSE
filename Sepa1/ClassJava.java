0cec0558b79f5f8e34e15da6ea31e23d73176c1b
joselucasoficial <joselucasoficial@gmail.com>
2022-03-16 23:48:52 -0300

    dda

--- Sepa1/ClassJava.java
+++ Sepa1/ClassJava.java
@@ -1,17 +1,17 @@
 public class ClassJava {
     public static void main(String[] args) {
+        int num = 0; // referencia-se ao numero de cada player
         
-        int num = 0; // para iniciar a contagem dos jogadores
-
         Player p1 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
+        p1.setLife(2);
         System.out.printf("a quantidade de vidas do Player %d é %d\n", num, p1.getLife() );
-
+        
         Player p2 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
         System.out.printf("a quantidade de vidas do Player %d é %d\n", num, p2.getLife() );
-
+        
         Player p3 = new Player(++num); // new passa os valores de (Player para a vareavel (p))
         System.out.printf("a quantidade de vidas do Player %d é %d\n",num, p3.getLife() );
-
+        
 
     }
 }
--- Sepa1/Player.java
+++ Sepa1/Player.java
@@ -1,15 +1,39 @@
-public class Player { // classe publica esta sendo acessada da classe ClassJava (main)
+public class Player { // classe publica 
 
-    private int num = 0;
+    private final int maxLife = 3;
+    private int num = 0; // vareavel local, só é exibita dentro da classe
     private int life = 0;
 
-    public Player(int num){  // classe construtora
-        this.num = num;
-        this.life = 3; 
+    public Player(int num){  // construtor esta sendo acessada da classe ClassJava (main)
+
+        this.num = num; // this referencia ao objeto da classe, no caso a vareavel num
+        this.addLife(3);
         //System.out.printf("jogador numero %d criado \n", num);
     }
 
     public int getLife(){ // metado tipo get pega o valor de alguma lugar
         return this.life; // retorna o valor de life passado no construtor
+    }
+
+    public void setLife(int life){ // metado tipo set
+        if(life > maxLife){
+            this.life=maxLife;
+        }else if(life < 0){
+            this.life=0;
+        }else{
+            this.life = life;
+        }
+        
+    }
+
+    private void addLife(int life){ // metado tipo set
+        if(life > maxLife){
+            this.life=maxLife;
+        }else if(life < 0){
+            this.life=0;
+        }else{
+            this.life = life;
+        }
+        
     }
 }
