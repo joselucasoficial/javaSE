dfe82be89c34a59d8fba2ac91250646e0f00e7b2
joselucasoficial <joselucasoficial@gmail.com>
2022-03-20 11:56:10 -0300

    matriz2

--- Matrizjava/Matriz.java
+++ Matrizjava/Matriz.java
@@ -2,12 +2,13 @@ public class Matriz{
     public static void main(String[] args) {
 
         // primeiro atributo de matriz
-        String[][] nomes = new String[3][4];
-        nomes[0][0] = "carros";
+        String[][] nomes = new String[3][4]; // atribuindo uma matriz com instanciação
+        nomes[0][0] = "carros"; // atribuindo "carros", na linha 0 na coluna 0
 
         // segundo atributo de matriz sem instanciação, direto na vareavel
+        // as linhas da matriz são criadas das seguintes formas {{linha 1},{linha 2}} separadas por virgulas, ja as colunas são atribuidas dentro das linhas e separadas por virgulas exemplo {{coluna 1, coluna 2}}.
         String[][] nomes2 = {{"calona 1", "coluna 2", "coluna 3"},{"calona 1", "coluna 2", "coluna"}};
-        System.out.println(nomes2[0][2]);
+        System.out.println(nomes2[0][2]); // impressão
         
     }
 }
